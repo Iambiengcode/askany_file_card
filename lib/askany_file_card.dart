3bd48aa50bd7cbf618be103ee00478ffe73bee8f
Trung Dang <trungdang29122000>
2022-10-31 16:56:58 +0700

    Export list file card

--- lib/askany_file_card.dart
+++ lib/askany_file_card.dart
@@ -1,3 +1,7 @@
 library askany_file_card;
 
+export 'package:askany_file_card/widgets/file_card.dart';
+export 'package:askany_file_card/widgets/list_file_card.dart';
+export 'package:askany_file_card/models/file_box_paramenters.dart';
+
 class AskanyFileCard {}
